fcc3c0a3976024e6567c810ebec907c8ee649b2e
Sharad Chauhan <>
2025-07-25 13:35:20 +0530

    added VaultViewModel to stitch store logic to UI

---
+++ Simply Notes/Views/AddNoteView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNoteView.swift
+//  Simply Notes
+//
+//  Created by Sharad Chauhan on 25/07/25.
+//
+
+import SwiftUI
+
+struct AddNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNoteView()
+}
---
+++ Simply Notes/Views/VaultView.swift
@@ -0,0 +1,18 @@
+//
+//  VaultView.swift
+//  Simply Notes
+//
+//  Created by Sharad Chauhan on 25/07/25.
+//
+
+import SwiftUI
+
+struct VaultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VaultView()
+}
